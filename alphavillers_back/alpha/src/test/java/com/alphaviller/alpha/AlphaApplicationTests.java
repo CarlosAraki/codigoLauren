ab38efe4fec58b8c3cc40d611a88eb2349799e83
Carlos Araki <carlos.araki@ws-solutions.com>
2023-03-19 19:39:56 -0300

    feat: adding backend springboot

---
+++ alphavillers_back/alpha/src/test/java/com/alphaviller/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphaviller.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
